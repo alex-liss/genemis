2d45ffda097f5a2807aa226d071dd0880b9f3344
alex.karely <alex.karely@serviceware.de>
2023-08-20 21:11:27 +0200

    added main layout

--- frontend/Genemis.Web/src/app/app.module.ts
+++ frontend/Genemis.Web/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/shell/header/header.component';
+import { FooterComponent } from './core/shell/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
