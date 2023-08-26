97b15d02d38b6f3c74e84b5d1075dca4b7adcd52
alex.karely <alex.karely@serviceware.de>
2023-08-26 20:50:48 +0200

    added flex layout

--- frontend/Genemis.Web/src/app/app.module.ts
+++ frontend/Genemis.Web/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/shell/header/header.component';
 import { FooterComponent } from './core/shell/footer/footer.component';
+import { MainAreaComponent } from './core/shell/main-area/main-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainAreaComponent
   ],
   imports: [
     BrowserModule
