a1a4d0fedb943687f850986adc0b34c3ca868394
alex.karely <alex.karely@serviceware.de>
2023-08-26 21:37:14 +0200

    added first routing and responsive layout

--- frontend/Genemis.Web/src/app/app.module.ts
+++ frontend/Genemis.Web/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/shell/header/header.component';
 import { FooterComponent } from './core/shell/footer/footer.component';
-import { MainAreaComponent } from './core/shell/main-area/main-area.component';
+import {RouterModule} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainAreaComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
