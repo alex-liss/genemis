6c084300af43e380ca7a2799a019e01189bf0ca9
alex-liss <alex.karely@proton.me>
2023-10-19 08:14:00 +0200

    import time series data with correct data type

--- database/MongoDB/init-data/2.init-col-messages.js
+++ database/MongoDB/init-data/2.init-col-messages.js
@@ -1,7 +1,5 @@
 db = db.getSiblingDB('genemis');
 
-//db.createCollection("messages");
-
 db.messages.insertMany([
     {key: "greeting", message: 'Welcome to Genemis', language: 'en'},
     {key: "greeting", message: 'Willkommen im Genemis', language: 'de'}]);
--- database/MongoDB/init-data/3.init-col-brent-europe.sh
+++ database/MongoDB/init-data/3.init-col-brent-europe.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-mongoimport --db genemis --collection ts_brent_europe --file /docker-entrypoint-initdb.d/Brent_Europe.csv --type tsv --headerline --username $GENEMIS_DB_USERNAME --password $GENEMIS_DB_PASSWORD
+mongoimport --db=genemis --collection=ts_brent_europe \
+--type=csv \
+--columnsHaveTypes \
+--fields="date.date(2006-01-02),price.double()" \
+--file=/docker-entrypoint-initdb.d/Brent_Europe.csv \
+--username=$GENEMIS_DB_USERNAME --password=$GENEMIS_DB_PASSWORD
